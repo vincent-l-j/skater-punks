eef318ec9dc7fdf10e80704824e21de390b23b71
Vincent Jadraque <vincent.jadraque@gmail.com>
2022-04-27 08:31:21 +1000

    Add NavBar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import NavBar from "./NavBar";
 
 function App() {
+  const [accounts, setAccounts] = useState([]);
   return (
     <div className="App">
+      <NavBar accounts={accounts} setAccounts={setAccounts} />
     </div>
   );
 }
