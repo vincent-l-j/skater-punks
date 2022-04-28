54f8583aa2dfb7afbe0eb23012b756ecd59881fa
Arkiea <arkie.ariyana1@gmail.com>
2022-04-28 19:28:17 +1000

    not working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { useState } from 'react';
+import React from 'react';
 import './App.css';
+import MainMint from './MainMint';
+import NavBar from './NavBar';
 
 function App() {
+  const [accounts, setAccounts] = useState([]);
+
   return (
-    <div className="App">
-    </div>
+  <div className="App">
+    <div>"Helllo"</div>
+    <NavBar accounts={accounts} setAccounts={setAccounts} />
+    <MainMint accounts={accounts} setAccounts={setAccounts} />
+  </div>
   );
 }
 
