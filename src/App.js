dcaf5330d477b4aa63f726444fd7f99ef852f32f
christianseeley@gmail.com <christianseeley@gmail.com>
2022-05-02 20:20:16 +1000

    made changes to front end

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import { useState } from "react";
-import "./App.css";
+import './App.css';
 import MainMint from "./MainMint";
-import NavBar from "./NavBar";
+import NavBar from './NavBar';
+
 
 function App() {
   const [accounts, setAccounts] = useState([]);
+  
   return (
     <div className="overlay">
-      <div className="App">
+      <div className="App">      
         <NavBar accounts={accounts} setAccounts={setAccounts} />
-        <MainMint accounts={accounts} setAccounts={setAccounts} />
+        <MainMint accounts={accounts} setAccounts={setAccounts} />      
       </div>
       <div className="moving-background"></div>
-    </div>
+    </div> 
   );
 }
 
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,69 +1,66 @@
 import React from "react";
-import { Box, Button, Flex, Image, Link, Spacer } from "@chakra-ui/react";
-import email from "./assets/social-media-icons/email_32x32.png"
-import facebook from "./assets/social-media-icons/facebook_32x32.png";
-import twitter from "./assets/social-media-icons/twitter_32x32.png"
+import { Box, Flex, Button, Image, Link, Spacer } from "@chakra-ui/react";
+import Facebook from "./assets/social-media-icons/facebook_32x32.png";
+import Twitter from "./assets/social-media-icons/twitter_32x32.png";
+import Email from "./assets/social-media-icons/email_32x32.png";
 
-const NavBar = ({ accounts , setAccounts }) => {
-  const isConnected = Boolean(accounts[0]);
+const NavBar = ({ accounts, setAccounts }) => {
+    const isConnected = Boolean(accounts[0]);
 
-  async function connectAccount() {
-    if (window.ethereum) {
-      const accounts = await window.ethereum.request({
-        method: "eth_requestAccounts",
-      });
-      setAccounts(accounts);
+    async function connectAccount() {
+        if (window.ethereum) {
+            const accounts = await window.ethereum.request({
+                method: "eth_requestAccounts",
+            });
+            setAccounts(accounts);
+        }
     }
-  }
 
-  return (
-    <Flex justify="space-between" align="center" padding="30px">
-      {/* Left side of NavBar  - Social Media Icons */}
-      <Flex justify="space-around" width="40%" padding="0 75px">
-        <Link href="https://www.facebook.com">
-          <Image src={facebook} boxSize="42px" margin="0 15px" />
-        </Link>
-        <Link href="https://www.twitter.com">
-          <Image src={twitter} boxSize="42px" margin="0 15px" />
-        </Link>
-        <Link href="https://www.gmail.com">
-          <Image src={email} boxSize="42px" margin="0 15px" />
-        </Link>
-      </Flex>
+    return (
+        <Flex justify="space-between" align="center" padding="30px">
+            {/* Left Side - Social Media Icons */}
+            <Flex justify="space-around" width="40%" padding="0 75px">
+                <Link href="https://www.facebook.com">
+                    <Image src={Facebook} boxSize="42px" margin="0 15px" />
+                </Link>
+                <Link href="https://www.twitter.com">
+                    <Image src={Twitter} boxSize="42px" margin="0 15px" />
+                </Link>
+                <Link href="https://www.gmail.com">
+                    <Image src={Email} boxSize="42px" margin="0 15px" />
+                </Link>
+            </Flex>
+                       
+            {/* Right Side - Sections and Connect */}
+            <Flex justify="space-around" align="center" width="40%" padding="30px">
+                <Box margin="0 15px" >Abount</Box>
+                <Spacer />
+                <Box margin="0 15px" >Mint</Box>
+                <Spacer />
+                <Box margin="0 15px" >Team</Box>
+                <Spacer />
 
-      {/* Right Side - Sections and Connect */}
-      <Flex
-        justify="space-around"
-        align="center"
-        width="40%"
-        padding="30px"
-      >
-        <Box margin="0 15px">About</Box>
-        <Spacer />
-        <Box margin="0 15px">Mint</Box>
-        <Spacer />
-        <Box margin="0 15px">Team</Box>
-        <Spacer />
-
-        {/* Connect */}
-        {isConnected ? (
-          <Box margin="0 15px">Connected</Box>
-        ): (
-          <Button
-            backgroundColor="darkgoldenrod"
-            borderRadius="5px"
-            boxShadow="0px 2px 2px 1px #0F0F0F"
-            color="black"
-            cursor="pointer"
-            fontFamily="inherit"
-            padding="15px"
-            margin="0 15px"
-            onClick={connectAccount}
-          >Connect Account</Button>
-        )}
-      </Flex>
-    </Flex>
-  )
+            {/* Connect */}
+            {isConnected ? (
+                <Box margin="0 15px">Connected</Box>
+            ) : (
+                <Button
+                    backgroundColor="#D6517D"
+                    borderRadius="5px"
+                    boxShadow="0px 2px 2px 1px #0F0F0F"
+                    color="white"
+                    cursor="pointer"
+                    fontFamily="inherit"
+                    padding="15px"
+                    margin="0 15pm"
+                    onClick={connectAccount}
+                >
+                    Connect
+                </Button>
+            )}
+        </Flex>
+    </Flex>        
+    )
 }
 
 export default NavBar;
